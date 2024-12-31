3cc171eef0bf59fce5594172715457000d6527d1
litfoot <sam_lightfoot@tamu.edu>
2024-12-31 09:50:57 +0800

    added more movies for step 5.1

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,10 @@ more_movies = [
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
     :release_date => '13-Aug-2021'}
+    { title: 'Inception', rating: 'PG-13', release_date: '16-Jul-2010' },
+    { title: 'The Dark Knight', rating: 'PG-13', release_date: '18-Jul-2008' },
+    { title: 'Interstellar', rating: 'PG-13', release_date: '07-Nov-2014' }
+  
 ]
 
 more_movies.each do |movie|
